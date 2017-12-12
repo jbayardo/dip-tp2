7162219725db4d113e344ad977712f8caaf521b0
Julián Bayardo Spadafora <julian@bayardo.info>
2017-12-12 17:49:49 -0300

    Compresión a color

--- src/compression.py
+++ src/compression.py
@@ -139,17 +139,16 @@ class SquareChunkProcessor(ImageProcessor):
 
     def apply(self, image):
         self.image = image
-        # TODO: hay que hacer que self._processed_image tenga el mismo tamanio que la nueva imagen
-        # self.image = np.pad(image,
-        #                     pad_width=[
-        #                         (0, self.chunk_size - (image.shape[0] % self.chunk_size)),
-        #                         (0, self.chunk_size - (image.shape[1] % self.chunk_size))],
-        #                     mode='constant')
         self.height = self.image.shape[0]
         self.width = self.image.shape[1]
 
         height_blocks = self.height // self.chunk_size
+        if self.height % self.chunk_size != 0:
+            height_blocks += 1
+
         width_blocks = self.width // self.chunk_size
+        if self.width % self.chunk_size != 0:
+            width_blocks += 1
 
         for height_block_index in range(height_blocks):
             for width_block_index in range(width_blocks):
@@ -195,7 +194,7 @@ class GreyscaleEncoder(Codec):
         return image
 
     def apply(self, image):
-        # TODO: assert image.dtype == np.uint8
+        assert image.dtype == np.uint8
 
         image = self._into_int_range(image)
         self._processed_image = np.zeros_like(image, dtype=np.double)
@@ -208,28 +207,6 @@ class GreyscaleEncoder(Codec):
 
     def _transform(self, chunk):
         return ffp.dct(ffp.dct(chunk.T, norm='ortho').T, norm='ortho')
-
-
-class GreyscaleDecoder(Codec):
-    @staticmethod
-    def _into_uint_range(image):
-        image = image + np.power(2, 7) * np.ones_like(image)
-        image = image.astype(np.uint8, copy=False)
-        return image
-
-    def apply(self, image):
-        assert image.dtype == np.uint8
-
-        self._processed_image = np.zeros_like(image)
-        super().apply(image)
-        self._processed_image = self._into_uint_range(self._processed_image)
-        return self._processed_image
-
-    def _quantize(self, chunk):
-        raise NotImplementedError()
-
-    def _transform(self, chunk):
-        return ffp.idct(ffp.idct(chunk.T, norm='ortho').T, norm='ortho')
 
 
 class TableGreyscaleEncoder(GreyscaleEncoder):
@@ -246,6 +223,26 @@ class TableGreyscaleEncoder(GreyscaleEncoder):
             chunk[chunk <= self._quant_threshold] = 0.0
 
         return chunk
+
+
+class GreyscaleDecoder(Codec):
+    @staticmethod
+    def _into_uint_range(image):
+        image = image + np.power(2, 7) * np.ones_like(image)
+        image = image.astype(np.uint8, copy=False)
+        return image
+
+    def apply(self, image):
+        self._processed_image = np.zeros_like(image)
+        super().apply(image)
+        self._processed_image = self._into_uint_range(self._processed_image)
+        return self._processed_image
+
+    def _quantize(self, chunk):
+        raise NotImplementedError()
+
+    def _transform(self, chunk):
+        return ffp.idct(ffp.idct(chunk.T, norm='ortho').T, norm='ortho')
 
 
 class TableGreyscaleDecoder(GreyscaleDecoder):
@@ -265,6 +262,7 @@ def zigzag(n):
     :param n: size of square matrix to iterate over
     :return: list of indexes in the matrix, sorted by visit order
     """
+
     def move(i, j):
         if j < (n - 1):
             return max(0, i - 1), j + 1
@@ -292,8 +290,8 @@ class DecompressingTableGreyscaleDecoder(TableGreyscaleDecoder):
         self._decompressor = decompressor
 
     def apply(self, image):
-        image = self._decompress(image)
-        super().apply(image)
+        decompressed = self._decompress(image)
+        super().apply(decompressed)
         return self._processed_image
 
     def _decompress(self, file):
@@ -413,28 +411,18 @@ class YCbCrSubsamplingEncoder(ImageProcessor):
         self._processors = processors
         self._sampling = sampling
 
-        self.height = None
-        self.width = None
-        self.image = None
-
     @staticmethod
     def rgb2ycbcr(im):
-        xform = np.array([[.299, .587, .114], [-.1687, -.3313, .5], [.5, -.4187, -.0813]])
+        xform = np.array([[.299, .587, .114], [-.168736, -.331264, .5], [.5, -.418688, -.081312]])
         ycbcr = im.dot(xform.T)
         ycbcr[:, :, [1, 2]] += 128
         return np.uint8(ycbcr)
 
-    @staticmethod
-    def ycbcr2rgb(im):
-        xform = np.array([[1, 0, 1.402], [1, -0.34414, -.71414], [1, 1.772, 0]])
-        rgb = im.astype(np.float)
-        rgb[:, :, [1, 2]] -= 128
-        return np.uint8(rgb.dot(xform.T))
-
     def chroma_subsample(self, subchannel, subsampling):
-        return skm.block_reduce(subchannel,
+        reduced = skm.block_reduce(subchannel,
                                 block_size=(subsampling[0], subsampling[1]),
-                                func=np.mean)
+                                func=np.median)
+        return np.uint8(reduced)
 
     def apply(self, image):
         channels = image.shape[2]
@@ -442,84 +430,211 @@ class YCbCrSubsamplingEncoder(ImageProcessor):
         assert 3 <= channels <= 4
         assert channels == 3 or 'A' in self._processors
 
-        self.image = image
-        self.height = self.image.shape[0]
-        self.width = self.image.shape[1]
+        image = image
+        height = image.shape[0]
+        width = image.shape[1]
 
         # Alpha subsampling
-        A_subsampled = None
+        A = None
         if channels == 4:
-            A_subsampled = self.chroma_subsample(self.image[:,:,3],
-                                                 self._sampling)
-            A_subsampled = self._processors['A'].apply(A_subsampled)
+            A = self.chroma_subsample(image[:, :, 3],
+                                      self._sampling)
+            A = self._processors['A'].apply(A)
 
-        ycbcr = YCbCrSubsamplingEncoder.rgb2ycbcr(self.image[:, :, :3])
+        ycbcr = YCbCrSubsamplingEncoder.rgb2ycbcr(image[:, :, :3])
 
-        Y = ycbcr[:,:,0]
+        Y = ycbcr[:, :, 0]
         Y = self._processors['Y'].apply(Y)
 
-        Cb_subsampled = self.chroma_subsample(ycbcr[:,:,1],
-                                              self._sampling)
-        Cb_subsampled = self._processors['Cb'].apply(Cb_subsampled)
+        Cb = self.chroma_subsample(ycbcr[:, :, 1],
+                                   self._sampling)
+        Cb = self._processors['Cb'].apply(Cb)
 
-        Cr_subsampled = self.chroma_subsample(ycbcr[:,:,2],
-                                              self._sampling)
-        Cr_subsampled = self._processors['Cr'].apply(Cr_subsampled)
+        Cr = self.chroma_subsample(ycbcr[:, :, 2],
+                                   self._sampling)
+        Cr = self._processors['Cr'].apply(Cr)
 
         return {
-            'height': self.height,
-            'width': self.width,
+            'height': height,
+            'width': width,
             'sampling': self._sampling,
             'Y': Y,
-            'Cb': Cb_subsampled,
-            'Cr': Cr_subsampled,
-            'A': A_subsampled
+            'Cb': Cb,
+            'Cr': Cr,
+            'A': A
         }
 
 
+class YCbCrSubsamplingDecoder(ImageProcessor):
+    def __init__(self, processors):
+        assert 'Y' in processors
+        assert 'Cb' in processors
+        assert 'Cr' in processors
+
+        self._processors = processors
+
+    @staticmethod
+    def ycbcr2rgb(im):
+        xform = np.array([[1, 0, 1.402], [1, -0.344136, -.714136], [1, 1.772, 0]])
+        rgb = im.astype(np.float)
+        rgb[:, :, [1, 2]] -= 128
+        return np.uint8(rgb.dot(xform.T))
+
+    def chroma_oversample(self, subchannel, image):
+        sampling = image['sampling']
+        oversampled = np.zeros(shape=(image['height'], image['width']))
+        for i in range(oversampled.shape[0]):
+            for j in range(oversampled.shape[1]):
+                oversampled[i][j] = subchannel[i//sampling[0]][j//sampling[1]]
+        return oversampled
+
+    def apply(self, image):
+        assert 'height' in image
+        assert 'width' in image
+        assert 'sampling' in image
+        assert 'Y' in image and image['Y'] is not None
+        assert 'Cb' in image and image['Cb'] is not None
+        assert 'Cr' in image and image['Cr'] is not None
+        assert 'A' in image and (image['A'] is None or 'A' in self._processors)
+
+        reconstructed = np.zeros(shape=(image['height'],
+                                        image['width'],
+                                        4))
+
+        Y = self._processors['Y'].apply(image['Y'])
+        reconstructed[:, :, 0] = Y
+
+        Cb = self._processors['Cb'].apply(image['Cb'])
+        Cb = self.chroma_oversample(Cb, image)
+        reconstructed[:, :, 1] = Cb
+
+        Cr = self._processors['Cr'].apply(image['Cr'])
+        Cr = self.chroma_oversample(Cr, image)
+        reconstructed[:, :, 2] = Cr
+
+        reconstructed[:, :, :3] = YCbCrSubsamplingDecoder.ycbcr2rgb(reconstructed[:, :, :3])
+
+        if image['A'] is not None:
+            A = self._processors['A'].apply(image['A'])
+            A = self.chroma_oversample(A, image)
+            reconstructed[:, :, 3] = A
+        else:
+            reconstructed = reconstructed[:, :, :3]
+
+        return reconstructed
+
+
+class Pad(ImageProcessor):
+    def __init__(self, multiple):
+        self._multiple = multiple
+
+    def apply(self, image):
+        pad0 = 0
+        if image.shape[0] % self._multiple != 0:
+            pad0 = self._multiple - (image.shape[0] % self._multiple)
+
+        pad1 = 0
+        if image.shape[1] % self._multiple != 0:
+            pad1 = self._multiple - (image.shape[1] % self._multiple)
+
+        pad_width = [(0, pad0), (0, pad1)]
+        while len(pad_width) < len(image.shape):
+            pad_width.append((0, 0))
+
+        old_dtype = image.dtype
+        image = np.pad(image,
+                       pad_width=pad_width,
+                       mode='constant',
+                       constant_values=0)
+        assert image.dtype == old_dtype
+
+        return image
+
+
+class Pipeline(ImageProcessor):
+    def __init__(self, *processors):
+        self._processors = processors
+
+    def apply(self, image):
+        for processor in self._processors:
+            image = processor.apply(image)
+        return image
+
+
+class KeepShape(ImageProcessor):
+    def __init__(self, inner):
+        self._inner = inner
+
+    def apply(self, image):
+        original_shape = image.shape
+
+        image = self._inner.apply(image)
+
+        # Crop image
+        assert len(original_shape) == len(image.shape)
+        assert len(original_shape) in [2, 3]
+        assert all([original_shape[i] <= image.shape[i] for i in range(len(original_shape))])
+
+        if len(original_shape) == 2:
+            image = image[:original_shape[0], :original_shape[1]]
+        else:
+            image = image[:original_shape[0], :original_shape[1], :original_shape[2]]
+
+        return image
+
+
 if __name__ == '__main__':
-    table = np.array([[17, 18, 24, 47, 99, 128, 192, 256],
-                      [18, 21, 26, 66, 99, 192, 256, 512],
-                      [24, 26, 56, 99, 128, 256, 512, 512],
-                      [47, 66, 99, 128, 256, 512, 1024, 1024],
-                      [99, 99, 128, 256, 512, 1024, 2048, 2048],
-                      [128, 192, 256, 512, 1024, 2048, 4096, 4096],
-                      [192, 256, 512, 1024, 2048, 4096, 8192, 8192],
-                      [256, 512, 512, 1024, 2048, 4096, 8192, 8192]])
+    quant_table = np.array([[17, 18, 24, 47, 99, 128, 192, 256],
+                            [18, 21, 26, 66, 99, 192, 256, 512],
+                            [24, 26, 56, 99, 128, 256, 512, 512],
+                            [47, 66, 99, 128, 256, 512, 1024, 1024],
+                            [99, 99, 128, 256, 512, 1024, 2048, 2048],
+                            [128, 192, 256, 512, 1024, 2048, 4096, 4096],
+                            [192, 256, 512, 1024, 2048, 4096, 8192, 8192],
+                            [256, 512, 512, 1024, 2048, 4096, 8192, 8192]])
 
     s = 7
-    table[0:s, 0:s] = np.ones_like(table[0:s, 0:s])
+    quant_table[0:s, 0:s] = np.ones_like(quant_table[0:s, 0:s])
 
-    table = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
-                      [12, 12, 14, 19, 26, 58, 60, 55],
-                      [14, 13, 16, 24, 40, 57, 69, 56],
-                      [14, 17, 22, 29, 51, 87, 80, 62],
-                      [18, 22, 37, 56, 68, 109, 103, 77],
-                      [24, 35, 55, 64, 81, 104, 113, 92],
-                      [49, 64, 78, 87, 103, 121, 120, 101],
-                      [72, 92, 95, 98, 112, 100, 103, 99]])
+    quant_table = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
+                            [12, 12, 14, 19, 26, 58, 60, 55],
+                            [14, 13, 16, 24, 40, 57, 69, 56],
+                            [14, 17, 22, 29, 51, 87, 80, 62],
+                            [18, 22, 37, 56, 68, 109, 103, 77],
+                            [24, 35, 55, 64, 81, 104, 113, 92],
+                            [49, 64, 78, 87, 103, 121, 120, 101],
+                            [72, 92, 95, 98, 112, 100, 103, 99]])
 
-    table = 1 * np.ones_like(table)
+    quant_table = 1 * np.ones_like(quant_table)
 
     # TODO: esto tendria que ser un buen compresor, hay que usar una tabla copada. Esta tabla flashea fuerte.
     translator = HuffmanTree.from_weight_dictionary({4: 54})
 
-    encoder = CompressingTableGreyscaleEncoder(translator, table)
-    decoder = DecompressingTableGreyscaleDecoder(translator, table)
+    grey_encoder = CompressingTableGreyscaleEncoder(translator, quant_table)
+    grey_encoder = Pipeline(Pad(8), grey_encoder)
+    grey_decoder = DecompressingTableGreyscaleDecoder(translator, quant_table)
 
-    color = ski.io.imread("D:\\jbayardo\\Documents\\dip-tp1\\data\\color\\1908iv.png")
+    colour_picture = ski.io.imread("D:\\jbayardo\\Documents\\dip-tp2\\test.jpg")
     rgb_encoder = YCbCrSubsamplingEncoder({
-        'Y': encoder,
-        'Cb': encoder,
-        'Cr': encoder,
-        'A': encoder
+        'Y': grey_encoder,
+        'Cb': grey_encoder,
+        'Cr': grey_encoder,
+        'A': grey_encoder
     }, (2, 2))
-    rgb_decoded = rgb_encoder.apply(color)
-    pass
+    rgb_encoder = Pipeline(Pad(8), rgb_encoder)
+    rgb_encoded = rgb_encoder.apply(colour_picture)
 
+    rgb_decoder = YCbCrSubsamplingDecoder({
+        'Y': grey_decoder,
+        'Cb': grey_decoder,
+        'Cr': grey_decoder,
+        'A': grey_decoder
+    })
+    rgb_decoded = rgb_decoder.apply(rgb_encoded)
+    ski.io.imshow(rgb_decoded)
+    plt.show()
     # lena = ski.io.imread("D:\\jbayardo\\Documents\\dip-tp1\\data\\test\\barbara.png")
     # encoded, compressed = encoder.apply(lena)
     # decoded = decoder.apply(compressed)
     # ski.io.imshow(decoded)
     # plt.show()
-
